e916fa4fbf138ed42b162512ca68241639d01377
Kaibling <l.rath@gmx.at>
2021-11-25 17:27:35 +0000

    cleanup

--- api/server.go
+++ api/server.go
@@ -3,7 +3,6 @@ package api
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -17,32 +16,22 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
+	"github.com/lucsky/cuid"
 )
 
 func AssembleServer() *chi.Mux {
 	configData := config.NewConfig()
 	configData.LogEnv()
-	sdb := database.NewDatabaseConnector(configData.DBUrl)
-	sdb.Connect()
-	sdb.Migrate(&models.User{})
-	sdb.Migrate(&models.Stock{})
-	sdb.Migrate(&models.StockToUser{})
-	sdb.Migrate(&models.Transaction{})
+	db := database.NewDatabaseConnector(configData.DBUrl)
+	db.Connect()
+	migrateDB(db)
 
-	userRepo := repositories.NewUserRepository(sdb)
-	repositories.SetUserRepo(userRepo)
-	stockRepo := repositories.NewStockRepository(sdb)
-	repositories.SetStockRepo(stockRepo)
-	transactionRepo := repositories.NewTransactionRepository(sdb)
-	repositories.SetTransactionRepo(transactionRepo)
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
-	r.Use(injectData("userRepo", userRepo))
-	r.Use(injectData("stockRepo", stockRepo))
-	r.Use(injectData("transactionRepo", transactionRepo))
-	r.Use(injectData("hmacSecret", []byte("asdassasdsdsdswew")))
+	r.Use(injectData("hmacSecret", []byte(config.Config.TokenSecret)))
+	initRepos(r, db)
 
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"*"},
@@ -52,48 +41,30 @@ func AssembleServer() *chi.Mux {
 		MaxAge:           300,
 	}))
 
-	testUser := &models.User{Username: "admin", Password: "testpassword"}
-	userRepo.Add(testUser)
-	token, _ := utility.GenerateToken(testUser.Username, []byte("asdassasdsdsdswew"))
-
 	BuildRouter(r)
-	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
-		log.Printf("%s %s\n", method, route)
-		return nil
-	}
-
-	if err := chi.Walk(r, walkFunc); err != nil {
-		fmt.Printf("Logging err: %s\n", err.Error())
-	}
-	fmt.Printf("user: %s\npassword:%s\ntoken:%s", "admin", "testpassword", token)
+	displayRoutes(r)
 	return r
 }
 func TestAssemblyRoute() (*chi.Mux, *repositories.UserRepository, *repositories.StockRepository, *repositories.TransactionRepository, func(r http.Handler, method, path string, jsonStr []byte) *httptest.ResponseRecorder) {
 	configData := config.NewConfig()
 	configData.LogEnv()
-	sdb := database.NewDatabaseConnector(configData.DBUrl)
-	sdb.Connect()
-	sdb.Migrate(&models.User{})
-	sdb.Migrate(&models.Stock{})
-	sdb.Migrate(&models.StockToUser{})
-	sdb.Migrate(&models.Transaction{})
+	db := database.NewDatabaseConnector(configData.DBUrl)
+	db.Connect()
+	token := migrateDB(db)
 
-	userRepo := repositories.NewUserRepository(sdb)
-
+	userRepo := repositories.NewUserRepository(db)
 	repositories.SetUserRepo(userRepo)
-	stockRepo := repositories.NewStockRepository(sdb)
+	stockRepo := repositories.NewStockRepository(db)
 	repositories.SetStockRepo(stockRepo)
-	transactionRepo := repositories.NewTransactionRepository(sdb)
+	transactionRepo := repositories.NewTransactionRepository(db)
 	repositories.SetTransactionRepo(transactionRepo)
+
 	r := chi.NewRouter()
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 	r.Use(injectData("userRepo", userRepo))
 	r.Use(injectData("stockRepo", stockRepo))
 	r.Use(injectData("transactionRepo", transactionRepo))
-	r.Use(injectData("hmacSecret", []byte("asdassasdsdsdswew")))
-	testUser := &models.User{Username: "testUser", Password: "testpassword"}
-	userRepo.Add(testUser)
-	token, _ := utility.GenerateToken(testUser.Username, []byte("asdassasdsdsdswew"))
+	r.Use(injectData("hmacSecret", []byte(config.Config.TokenSecret)))
 	BuildRouter(r)
 	PerformTestRequest := func(r http.Handler, method, path string, jsonStr []byte) *httptest.ResponseRecorder {
 
@@ -115,5 +86,48 @@ func injectData(key string, data interface{}) func(next http.Handler) http.Handl
 			next.ServeHTTP(w, r.WithContext(ctx))
 		}
 		return http.HandlerFunc(fn)
+	}
+}
+
+func migrateDB(db database.DBConnector) string {
+
+	db.Migrate(&models.User{})
+	db.Migrate(&models.Stock{})
+	db.Migrate(&models.StockToUser{})
+	db.Migrate(&models.Transaction{})
+
+	if err := db.FindByWhere(&models.User{}, "username = ?", []interface{}{"admin"}); err != nil {
+		log.Println("no admin found. Creating one")
+		password := cuid.New()
+		admin := &models.User{Username: "admin", Password: password, Email: "admin@local"}
+		db.Add(admin)
+		token, _ := utility.GenerateToken(admin.Username, []byte(config.Config.TokenSecret))
+		log.Printf("user: %s, password: %s\ntoken: %s\n", admin.Username, password, token)
+		return token
+	}
+	return ""
+}
+
+func initRepos(r *chi.Mux, db database.DBConnector) {
+	userRepo := repositories.NewUserRepository(db)
+	repositories.SetUserRepo(userRepo)
+	stockRepo := repositories.NewStockRepository(db)
+	repositories.SetStockRepo(stockRepo)
+	transactionRepo := repositories.NewTransactionRepository(db)
+	repositories.SetTransactionRepo(transactionRepo)
+
+	r.Use(injectData("userRepo", userRepo))
+	r.Use(injectData("stockRepo", stockRepo))
+	r.Use(injectData("transactionRepo", transactionRepo))
+}
+
+func displayRoutes(r *chi.Mux) {
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route)
+		return nil
+	}
+
+	if err := chi.Walk(r, walkFunc); err != nil {
+		log.Printf("Logging err: %s\n", err.Error())
 	}
 }
--- lib/config/config.go
+++ lib/config/config.go
@@ -9,23 +9,35 @@ var OSPREFIX = "POS"
 var Config *Configuration
 
 type Configuration struct {
-	DBUrl string
+	DBUrl       string
+	TokenSecret string
 }
 
 func NewConfig() *Configuration {
-	dbURL := os.Getenv(OSPREFIX + "_DBURL")
-	if dbURL == "" {
-		//path, err := os.Getwd()
-		//if err != nil {
-		//	log.Println(err)
-		//}
-		//dbURL = filepath.Join(path, "local.db")
-		dbURL = "file::memory:"
-	}
-	Config = &Configuration{DBUrl: dbURL}
-	return &Configuration{DBUrl: dbURL}
+	//path, err := os.Getwd()
+	//if err != nil {
+	//	log.Println(err)
+	//}
+	//dbURL = filepath.Join(path, "local.db")
+	dbURL := getEnv("DBURL", "file::memory:")
+	tokenSecret := getEnv("TOKEN_SECRECT", "tokensecretreally")
+	Config = &Configuration{DBUrl: dbURL, TokenSecret: tokenSecret}
+	return Config
 }
 
 func (s *Configuration) LogEnv() {
 	log.Println("DBURL: " + s.DBUrl)
+}
+
+func getEnv(key string, defaultValue string) string {
+	fullKey := OSPREFIX + "_" + key
+	val := os.Getenv(OSPREFIX + "_" + key)
+	if val == "" {
+		if defaultValue != "" {
+			return defaultValue
+		}
+		panic(fullKey + " is not set")
+	}
+	return val
+
 }
